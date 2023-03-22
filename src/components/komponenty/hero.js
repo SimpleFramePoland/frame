7a7ae8c0f15e9b834fa3edd3515b9a2ce9ad6db6
La100 <stolamarcin@gmail.com>
2023-03-22 14:53:11 +0100

    elo

--- src/components/komponenty/hero.js
+++ src/components/komponenty/hero.js
@@ -4,7 +4,7 @@ export default function Hero(){
  return (
    <div className="relative ">
    <div className="bg-hero-section bg-no-repeat h-screen bg-center bg-cover ">
-     <video autoPlay loop muted playsinline className="absolute inset-0 object-cover w-screen h-screen xl:h-auto grayscale">
+     <video autoPlay loop muted playsInline className="absolute inset-0 object-cover w-screen h-screen xl:h-auto grayscale">
        <source
          src="./../video3.mp4"
          type="video/mp4"
