5f85db87eb66f35a511713930db3951a14cddfdf
La100 <stolamarcin@gmail.com>
2023-03-22 11:51:57 +0100

    elo

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,9 @@ module.exports = {
         "es2021": true
     },
     "extends": [
+        
         "plugin:react/recommended",
-        "next/core-web-vitals",
+        "plugin:@next/next/recommended",
       
     ],
     "overrides": [
