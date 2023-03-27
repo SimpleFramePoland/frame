d37a07c876ed79066d8b03a03c8fed501a0fa130
La100 <stolamarcin@gmail.com>
2023-03-27 14:48:54 +0200

    elo

--- src/pages/categories/[slug].js
+++ src/pages/categories/[slug].js
@@ -1,11 +1,12 @@
-import { getAllCategories, getCategoryBySlug } from 'lib/categories';
+import { getCategoryBySlug } from 'lib/categories';
 import { getPostsByCategoryId } from 'lib/posts';
 import usePageMetadata from 'hooks/use-page-metadata';
 import React from 'react';
 import TemplateArchive from 'templates/archive';
 import Title from 'components/Title';
+import { getAllCategories } from 'lib/categories';
 
-export default function Category({ category, posts }) {
+export default function Category({ categories, category, posts }) {
   const { name, description, slug } = category;
 
   const { metadata } = usePageMetadata({
@@ -15,7 +16,7 @@ export default function Category({ category, posts }) {
     },
   });
 
-  return <TemplateArchive title={name} Title={<Title title={name} />} posts={posts} slug={slug} metadata={metadata} />;
+  return <TemplateArchive categories={categories} title={name} Title={<Title title={name} />} posts={posts} slug={slug} metadata={metadata} />;
 }
 
 export async function getStaticProps({ params = {} } = {}) {
@@ -27,7 +28,7 @@ export async function getStaticProps({ params = {} } = {}) {
       notFound: true,
     };
   }
-
+  const { categories } = await getAllCategories();
   const { posts } = await getPostsByCategoryId({
     categoryId: category.databaseId,
     queryIncludes: 'archive',
@@ -35,6 +36,7 @@ export async function getStaticProps({ params = {} } = {}) {
 
   return {
     props: {
+      categories,
       category,
       posts,
     },
@@ -42,19 +44,32 @@ export async function getStaticProps({ params = {} } = {}) {
 }
 
 export async function getStaticPaths() {
-  const { categories } = await getAllCategories();
 
-  const paths = categories.map((category) => {
-    const { slug } = category;
-    return {
-      params: {
-        slug,
-      },
-    };
-  });
+  // we're considering them non-critical pages
+
+  // To enable pre-rendering of Category pages:
+
+  // 1. Add import to the top of the file
+  //
+  // import { getAllCategories, getCategoryBySlug } from 'lib/categories';
+
+  // 2. Uncomment the below
+  //
+  // const { categories } = await getAllCategories();
+
+  // const paths = categories.map((category) => {
+  //   const { slug } = category;
+  //   return {
+  //     params: {
+  //       slug,
+  //     },
+  //   };
+  // });
+
+  // 3. Update `paths` in the return statement below to reference the `paths` constant above
 
   return {
-    paths,
+    paths: [],
     fallback: 'blocking',
   };
 }
