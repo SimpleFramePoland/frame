40087979cd3ecfa9649308653745df4af83eca4d
La100 <stolamarcin@gmail.com>
2023-03-28 13:25:50 +0200

    elo

--- src/pages/kategoria/[slug].js
+++ src/pages/kategoria/[slug].js
@@ -4,11 +4,12 @@ import usePageMetadata from 'hooks/use-page-metadata';
 import React from 'react';
 import TemplateArchive from 'templates/archive';
 import Title from 'components/Title';
-
+import Link from 'next/link';
 import Container from 'components/Container';
-
-
-export default function Category({ categories, category, posts,page }) {
+import { categoryPathBySlug } from 'lib/categories';
+import PostCard from 'components/PostCard';
+import Layout from 'components/Layout';
+export default function Category({ categories, category, posts, page }) {
   const { name, description, slug } = category;
 
   const { metadata } = usePageMetadata({
@@ -19,18 +20,51 @@ export default function Category({ categories, category, posts,page }) {
   });
 
   return (
-  <>
- 
-   <Container>
- 
-       <h1 className="text-3xl font-bold mb-6 ">{name}</h1>
-             {description && <p className="text-xl mb-6">{description}</p>}
-         
-             </Container>     
-             
-  <TemplateArchive categories={categories} title={name} Title={<Title title={name} />} posts={posts} slug={slug} metadata={metadata} />; 
-  
-  </>)
+    <>
+      <Layout>
+        <Container>
+
+
+
+
+          <div className='flex  px-2 mt-[8rem] px-12 justify-center  '>
+            <div className='flex-1 flex-none'>
+              {categories && categories.length > 0 ? (
+                <ul className='border-4 border-babyblue max-w-[13rem] lg:max-w-full rounded-xl'>
+                  {categories.map((category) => (
+                    <li key={category.slug}>
+                      <Link href={categoryPathBySlug(category.slug)}>
+                        {category.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p></p>
+              )}
+            </div>
+            <div className='flex-2'>
+              {description ? (
+                <p className="text-xl mb-6">{description}</p>
+              ) : (
+                <p className="text-xl mb-6"></p>
+              )}
+              <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8 mx-auto">
+                {posts.map((post) => {
+                  return (
+                    <li key={post.slug}>
+                      <PostCard post={post} />
+                    </li>
+                  );
+                })}
+              </ul>
+
+            </div>
+          </div>
+
+        </Container>
+      </Layout>
+    </>)
 }
 
 export async function getStaticProps({ params = {} } = {}) {
@@ -51,7 +85,7 @@ export async function getStaticProps({ params = {} } = {}) {
 
   return {
     props: {
-   
+
       categories,
       category,
       posts,
@@ -60,17 +94,17 @@ export async function getStaticProps({ params = {} } = {}) {
 }
 export async function getStaticPaths() {
 
-  
- const { categories } = await getAllCategories();
+
+  const { categories } = await getAllCategories();
 
   const paths = categories.map((category) => {
-   const { slug } = category;
+    const { slug } = category;
     return {
-    params: {
-      slug,
-     },
-  };
-});
+      params: {
+        slug,
+      },
+    };
+  });
 
   // 3. Update `paths` in the return statement below to reference the `paths` constant above
 
