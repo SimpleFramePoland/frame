ec2cad76dbdc1739d27def696b556c6f09233b9f
La100 <stolamarcin@gmail.com>
2023-03-31 15:04:26 +0200

    elo

--- src/pages/kategoria/[slug].js
+++ src/pages/kategoria/[slug].js
@@ -92,21 +92,21 @@ export async function getStaticProps({ params = {} } = {}) {
 export async function getStaticPaths() {
 
   
-  const { categories } = await getAllCategories();
+ // const { categories } = await getAllCategories();
 
- const paths = categories.map((category) => {
-   const { slug } = category;
- return {
-     params: {
-     slug,
-   },
-   };
-});
+ //const paths = categories.map((category) => {
+//   const { slug } = category;
+// return {
+ //    params: {
+//     slug,
+  // },
+ //  };
+//});
 
  // 3. Update `paths` in the return statement below to reference the `paths` constant above
 
  return {
-   paths,
+   paths : [],
    fallback: 'blocking',
  };
 }
