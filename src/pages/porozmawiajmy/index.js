29251e5630488151f702147f680747cee1f49cf0
La100 <stolamarcin@gmail.com>
2023-04-03 14:56:59 +0200

    elo

--- src/pages/porozmawiajmy/index.js
+++ src/pages/porozmawiajmy/index.js
@@ -12,8 +12,9 @@ export default function Home() {
     <Layout>
       <Carousel/>
       <Container>
-      <div className='flex flex-col md:flex-row  mt-[120px] '>
-      <div>
+      <ContactForm/>
+      <div className='flex flex-col md:flex-row flex-auto  pt-12 '>
+      <div >
          
       <h3>Simple Frame</h3>
         <p>
@@ -25,12 +26,12 @@ ul. Wiertnicza 135A
 <a href="mailto:kontakt@simpleframe.pl">kontakt@simpleframe.pl</a>
 </p>
       </div>
-        <div>
-     <ContactForm/>
+        <div className='flex-auto'>
+  
       
       </div>
       
-      <div >
+      <div className='s'>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2446.690595269549!2d21.072874915794703!3d52.17631867975067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecd51214093e5%3A0xa8e9b221662996cd!2sSimple%20Frame!5e0!3m2!1spl!2spl!4v1679660118254!5m2!1spl!2spl" className='mx-auto' width="600" height="450"   loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
