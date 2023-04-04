63fbdddbdad76f035f155998bf6bf83e66b26520
La100 <stolamarcin@gmail.com>
2023-04-04 10:23:59 +0200

    elo

--- src/components/komponenty/hero.js
+++ src/components/komponenty/hero.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Hero(){ 
  return (
    <div className="relative bg-black ">
-   <div className="bg-hero-section bg-no-repeat h-screen bg-center bg-cover ">
+   <div className="bg-hero-section bg-no-repeat h-screen bg-center bg-cover  ">
     <h1 className='text-white text-5xl text-center z-10 pt-[14rem]'>Produkcja filmowa. Produkcja Reklam. Animacje</h1>
-   <video autoPlay loop muted playsInline className="absolute inset-0 object-cover w-screen h-screen xl:h-auto grayscale">
+   <video autoPlay loop muted playsInline className="absolute inset-0 object-cover w-screen h-screen xl:h-auto  grayscale">
        <source
          src="./../edit2.mp4"
          type="video/mp4"
