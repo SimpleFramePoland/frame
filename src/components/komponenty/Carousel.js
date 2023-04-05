e28d5f074c048f1a3c9f559f7b3701bcfb6e5387
La100 <stolamarcin@gmail.com>
2023-04-05 16:06:45 +0200

    elo

--- src/components/komponenty/Carousel.js
+++ src/components/komponenty/Carousel.js
@@ -5,7 +5,7 @@ export default function Slider() {
     <div className=" bg-black">
     <video autoPlay loop muted playsInline className=" w-screen pt-[5rem]  ">
        <source
-         src="/edit1.mp4"
+         src="/Edit.mp4"
          type="video/mp4"
        />
      </video>
--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -9,20 +9,35 @@ async function sendEmail(req, res) {
     await sendgrid.send({
       to: "admin@simpleframe.pl", // Your email where you'll receive emails
       from: "admin@simpleframe.pl", // your website email address here
-      replyTo:`${req.body.email}`,
-      subject: `Wiadomość od  : ${req.body.email}`,
-      html: `
+      subject: `[Lead from website] : ${req.body.subject}`,
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+      </head>
+
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
+              </div>
               <div class="container" style="margin-left: 20px;margin-right: 20px;">
-              <h3>Wiadmość od ${req.body.fullname}, email: ${req.body.email} </h3>
+              <h3>You've got a new mail from ${req.body.fullname}, their email is: ✉️${req.body.email} </h3>
               <div style="font-size: 16px;">
-              <p>Wiadomość:</p>
+              <p>Message:</p>
               <p>${req.body.message}</p>
               <br>
               </div>
-              
-            
+             
               </div>
-     `,
+      </body>
+      </html>`,
     });
   } catch (error) {
     // console.log(error);
