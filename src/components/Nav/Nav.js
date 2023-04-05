2134a35e9a769dfc2200370065eed26dbb8573a0
La100 <stolamarcin@gmail.com>
2023-04-05 13:45:11 +0200

    elo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -333,7 +333,7 @@ export default function Header() {
                   )}
                 </Disclosure>
                 <a
-                  href="#"
+                  href="/postprodukcja"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white hover:bg-black-50"
                 >
                   Postprodukcja
