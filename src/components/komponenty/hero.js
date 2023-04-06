b13020d2350065b749be83054da1e950c1d7a9b6
La100 <stolamarcin@gmail.com>
2023-04-06 13:34:52 +0200

    elo

--- src/components/komponenty/hero.js
+++ src/components/komponenty/hero.js
@@ -4,14 +4,43 @@ export default function Hero(){
  return (
 
    <div className="relative flex items-center justify-center bg-black bg-no-repeat h-screen  bg-cover  ">
-      <h1 className='px-8 z-20 text-6xl lg:text-8xl h-[11rem] flex items-center  align-middle font-bold text-white  rounded-xl text-center'>Stworzymy Twój Film</h1>
-   <video autoPlay loop muted playsInline className="absolute bg-clip-text inset-0 object-cover  w-screen h-screen xl:h-auto  ">
+  <div className="herotext flex flex-col items-center" id="first">
+  	<h1 className='text-5xl lg:text-7xl text-center font-bold drop-shadow-xl' >Wyprodukujemy Twój film</h1>
+   <p className='text-center font-bold drop-shadow-xl pt-6 text-2xl'> Zrealizujemy Twój film w rozdzielczości HD lub 4K</p>
+   <button className='btn mt-7 w-[7rem] h-10 mx-auto bg-babyblue rounded-lg '>
+    <a className="text-white text-xl font"href="/porozmawiajmy">
+      Kontakt
+    </a>
+   </button>
+  </div>
+  <div className="herotext flex flex-col items-center " id="second">
+  	<h1 className='text-5xl lg:text-7xl text-center font-bold drop-shadow-xl'>Stworzymy Twoją animację</h1>
+    <p className='text-center font-bold drop-shadow-xl pt-6 text-2xl'>Potencjał animacji wciąż rośnie</p>
+    <button className='btn mt-7 w-[7rem] h-10 mx-auto bg-babyblue rounded-lg '>
+    <a className="text-white text-xl font"href="/porozmawiajmy">
+      Kontakt
+    </a>
+   </button>
+  </div>
+  <div className="herotext flex flex-col items-center" id="third">
+  	<h1 className='text-5xl lg:text-7xl text-center font-bold drop-shadow-xl'>Zaprojektujemy Twoją reklamę</h1>
+    <p className='text-center font-bold drop-shadow-xl pt-6 text-2xl'> Pomożemy Ci zaskoczyć Twoich kontrahentów</p>
+    <button className='btn mt-7 w-[7rem] h-10 mx-auto bg-babyblue rounded-lg '>
+    <a className="text-white text-xl font"href="/porozmawiajmy">
+      Kontakt
+    </a>
+   </button>
+  </div>
+  <video autoPlay loop muted playsInline className="absolute bg-clip-text inset-0 object-cover  w-screen h-screen xl:h-auto  ">
        <source
          src="./../edit2.mp4"
          type="video/mp4"
        />
      </video>
-   
+
+   <style>
+
+   </style>
    </div>
 
  )
