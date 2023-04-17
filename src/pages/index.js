9ea22bfa8608bd3ba275ba9ced974dafb2d7c2bb
La100 <stolamarcin@gmail.com>
2023-04-17 12:59:03 +0200

    elo

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,12 +27,12 @@ export default function Home({page}) {
         <meta name="twitter:description" content={description} />
         </Head>
         
-           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
+     
     
      
 
-       
-<script async src='/js/fancybox.js' beforeInteractive id='modula-fancybox-js'/>
+        <script async src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" /> 
+<script async src='/js/fancybox.js' id='modula-fancybox-js'/>
 
 <link rel='stylesheet' id='modula-video-css-css' href='https://old.simpleframe.pl/wp-content/plugins/modula-video/assets/css/modula-video-css.css?ver=6.1.1' type='text/css' media='all' />
 <link rel='stylesheet' id='modula-css' href='https://old.simpleframe.pl/wp-content/plugins/modula-best-grid-gallery/assets/css/front.css?ver=2.7.3' type='text/css' media='all' />
