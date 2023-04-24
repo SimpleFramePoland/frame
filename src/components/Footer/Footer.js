baf72b837be1072f556c92803748606939098fc4
La100 <stolamarcin@gmail.com>
2023-04-24 15:48:29 +0200

    elo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 
 import React from 'react';
 import Image from 'next/image';
 import Section from 'components/Section';
-import Link from 'next/link';
+
 
 
 
@@ -18,8 +19,8 @@ const Footer = () => {
           <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
     <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
     <div className="flex lg:flex-1">
-        <Link href="/" className=" ">
-         
+        <a href="/" className=" ">
+          <span className="sr-only">Simple frame</span>
           <Image
       src="/logofooter.png"
       alt="Logo simpleframe"
@@ -27,7 +28,7 @@ const Footer = () => {
       height={45}
       className="lg:-10"
     />
-        </Link >
+        </a>
        
       </div>
       <p className="mt-4  ">
@@ -35,7 +36,7 @@ const Footer = () => {
  
         ul. Wiertnicza 135A<br/>
 02-952 Warszawa<br/>
-<a className="text-babyblue" href="tel:+48 502 061 431">+48 502 061 431</a > <br />
+<a className="text-babyblue" href="tel:+48 502 061 431">+48 502 061 431</a> <br />
                   <a className="text-babyblue" href="mailto:kontakt@simpleframe.pl">kontakt@simpleframe.pl</a> <br/>
 
 
@@ -46,45 +47,47 @@ const Footer = () => {
         <h3 className="title-font font-bold text-babyblue tracking-widest text-sm mb-3">Produkcja filmowa </h3>
         <nav className="list-none mb-10">
           <li>
-            <Link href='/film-korporacyjny'><a className="text-white hover:text-babyblue">Film Korporacyjny</a></Link >
+            <a href="/film-reklamowy" className="text-white hover:text-babyblue">Film Reklamowy</a>
           </li>
           <li>
-            <Link href='/film-promocyjny' ><a className="text-white hover:text-babyblue">Film Promocyjny</a></Link >
+            <a href='/film-korporacyjny' className="text-white hover:text-babyblue">Film Korporacyjny</a>
           </li>
           <li>
-            <Link href='/film-produktowy'><a  className="text-white hover:text-babyblue" >Film Produktowy</a></Link >
-          </li>
-        </nav>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h3 className='mb-3' ><a className="title-font font-bold text-babyblue tracking-widest text-sm mb-3">Animacje </a></h3>
-        <nav className="list-none mb-10">
-      
-          <li>
-            <Link href= '/animacja-3d-2'><a  className="text-white hover:text-babyblue" >Animacja 3D</a></Link >
+            <a href='/film-promocyjny' className="text-white hover:text-babyblue">Film Promocyjny</a>
           </li>
           <li>
-            <Link href='/animacja-2d' ><a className="text-white hover:text-babyblue" >Animacja 2D</a></Link >
-          </li>
-          <li>
-            <Link href='/animacja-techniczna' ><a className="text-white hover:text-babyblue">Animacja Techniczna </a></Link >
-          </li>
-          <li>
-            <Link href= '/animacja-produktowa' ><a className="text-white hover:text-babyblue" >Animacja Produktowa </a></Link>
+            <a href='/film-produktowy' className="text-white hover:text-babyblue">Film Produktowy</a>
           </li>
         </nav>
       </div>
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h3 className='mb-3'><a className="title-font font-bold text-babyblue tracking-widest text-sm mb-3" >Postprodukcja</a></h3>
+        <h3 className="title-font font-bold text-babyblue tracking-widest text-sm mb-3">Animacje</h3>
         <nav className="list-none mb-10">
           <li>
-            <Link href="/postprodukcja"><a className="text-white hover:text-babyblue">Postprodukcja</a></Link >
+            <a href= '/animacja-3d-2/' className="text-white hover:text-babyblue">Animacja 3D</a>
           </li>
           <li>
-            <Link href="/montaz-filmow" ><a className="text-white hover:text-babyblue">Montaż filmów</a></Link >
+            <a href= '/animacja-2d' className="text-white hover:text-babyblue">Animacja 2D</a>
           </li>
           <li>
-            <Link href='/produkcja-animacji' ><a className="text-white hover:text-babyblue" >Produkcja Animacji</a></Link >
+            <a href= '/animacja-techniczna' className="text-white hover:text-babyblue">Animacja Techniczna</a>
+          </li>
+          <li>
+            <a  href= '/animacja-produktowa' className="text-white hover:text-babyblue">Animacja Produktowa</a>
+          </li>
+        </nav>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+        <h3 className="title-font font-bold text-babyblue tracking-widest text-sm mb-3">Postprodukcja</h3>
+        <nav className="list-none mb-10">
+          <li>
+            <a href="/postprodukcja"className="text-white hover:text-babyblue">Postprodukcja</a>
+          </li>
+          <li>
+            <a href="/montaz-filmow" className="text-white hover:text-babyblue">Montaż filmów</a>
+          </li>
+          <li>
+            <a href='/produkcja-animacji' className="text-white hover:text-babyblue">Produkcja Animacji</a>
           </li>
         
         </nav>
@@ -93,16 +96,16 @@ const Footer = () => {
         <h3 className="title-font font-bold text-babyblue tracking-widest text-sm mb-3">Usługi</h3>
         <nav className="list-none mb-10">
           <li>
-            <Link href= '/billboard-sponsorski' ><a className="text-white hover:text-babyblue" >Billboard Sponsorski</a></Link >
+            <a href= '/billboard-sponsorski' className="text-white hover:text-babyblue">Billboard Sponsorski</a>
           </li>
           <li>
-            <Link href= '/teledyski' ><a className="text-white hover:text-babyblue" >Teledyski</a></Link >
+            <a href= '/teledyski' className="text-white hover:text-babyblue">Teledyski</a>
           </li>
           <li>
-            <Link  href= '/fotografia' ><a className="text-white hover:text-babyblue" >Fotografia</a></Link >
+            <a  href= '/fotografia' className="text-white hover:text-babyblue">Fotografia</a>
           </li>
           <li>
-            <Link href='/streaming-video' ><a className="text-white hover:text-babyblue" >Streaming</a></Link >
+            <a href='/streaming-video' className="text-white hover:text-babyblue">Streaming</a>
           </li>
         </nav>
       </div>
@@ -110,7 +113,7 @@ const Footer = () => {
   </div>
   
     <div className="container mx-auto  px-5 flex flex-wrap flex-col sm:flex-row">
-      <p className="text-white fonttext-sm text-center sm:text-left">© 2023 SimpleFrame Sp. z o.o. 
+      <p className="text-babyblue fonttext-sm text-center sm:text-left">© 2023 SimpleFrame
         
       </p>
      
