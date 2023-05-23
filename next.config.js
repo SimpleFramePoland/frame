460f84eaab72ec1e57be7d8d2bb59469f983a984
La100 <stolamarcin@gmail.com>
2023-05-23 10:45:10 +0200

    brre

--- next.config.js
+++ next.config.js
@@ -21,6 +21,117 @@ module.exports = () => {
           destination: '/posts/:slug',
           permanent: true,
         },
+        {
+          source: '/portfolio/:slug',
+          destination: '/posts/:slug',
+          permanent: true,
+        },
+        {
+          source: '/film-wizerunkowy-eurocash',
+          destination: '/posts/film-wizerunkowy-eurocash',
+          permanent: true,
+        },
+        {
+          source: '/sciton-prezentacja-3d',
+          destination: '/posts/sciton-prezentacja-3d',
+          permanent: true,
+        },
+        {
+          source: '/betfan-reklama-telewizyjna/',
+          destination: '/posts//betfan-reklama-telewizyjna/',
+          permanent: true,
+        },
+        {
+          source: '/pastelowa-animacja-3d-mpack/',
+          destination: '/posts/pastelowa-animacja-3d-mpack/',
+          permanent: true,
+        },
+        {
+          source: '/polbram-animacja-3d-produktu/',
+          destination: '/posts/polbram-animacja-3d-produktu/',
+          permanent: true,
+        },
+        {
+          source: '/animacje-reklamowe/',
+          destination: '/produkcja-animacji/',
+          permanent: true,
+        },
+        {
+          source: '/animacja-trojwymiarowa/',
+          destination: '/animacja-3d/',
+          permanent: true,
+        },
+        {
+          source: '/mlekovita/mlekovita_ik01/',
+          destination: '/posts/mlekovita/',
+          permanent: true,
+        },
+        {
+          source: '/animacja-2d-playtegra/',
+          destination: '/posts/animacja-2d-playtegra/',
+          permanent: true,
+        },
+        {
+          source: '/prestige-vt/',
+          destination: '/posts/prestige-vt/',
+          permanent: true,
+        },
+        {
+          source: '/film-promocyjny-3/',
+          destination: '/posts/film-promocyjny-3/',
+          permanent: true,
+        },
+        {
+          source: '/lufthansa/',
+          destination: '/posts/lufthansa/',
+          permanent: true,
+        },
+        {
+          source: '/hh/',
+          destination: '/posts/hh/',
+          permanent: true,
+        },
+        {
+          source: '/zelmer/',
+          destination: '/posts/zelmer/',
+          permanent: true,
+        },
+        {
+          source: '/klimatherm-film-produktowy/',
+          destination: 'posts/klimatherm-film-produktowy/',
+          permanent: true,
+        },
+        {
+          source: '/produkcja-filmowa-polaczenie-doswiadczenia-z-kreatywnoscia/',
+          destination: '/posts/produkcja-filmowa-polaczenie-doswiadczenia-z-kreatywnoscia/',
+          permanent: true,
+        },
+        {
+          source: '/ey-business-advisory/',
+          destination: '/posts/ey-business-advisory/',
+          permanent: true,
+        },
+        {
+          source: '/badz-na-czasie-pokaz-sie-na-instagramie/',
+          destination: '/posts/badz-na-czasie-pokaz-sie-na-instagramie/',
+          permanent: true,
+        },
+        {
+          source: '/streaming-video-arabiantravelmarket/',
+          destination: '/posts/streaming-video-arabiantravelmarket/',
+          permanent: true,
+        },
+        {
+          source: '/hotel-spa/',
+          destination: '/posts/hotel-spa/',
+          permanent: true,
+        },
+        {
+          source: '/beko/',
+          destination: '/posts/beko/',
+          permanent: true,
+        },
+
       ];
     },
     basePath: '',
