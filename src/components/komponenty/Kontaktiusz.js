197d50f42dbb037dcb87d40dee23d84323fb46a4
SimpleFramePoland <b.osmulski@simpleframe.pl>
2024-12-12 20:24:12 +0100

    Update Kontaktiusz.js

--- src/components/komponenty/Kontaktiusz.js
+++ src/components/komponenty/Kontaktiusz.js
@@ -26,9 +26,7 @@ ul. ul. Gawędziarzy 2
       
       </div>
       
-      <div >
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2446.690595269549!2d21.072874915794703!3d52.17631867975067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecd51214093e5%3A0xa8e9b221662996cd!2sSimple%20Frame!5e0!3m2!1spl!2spl!4v1679660118254!5m2!1spl!2spl" className='mx-auto' width="600" height="450"   loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-      </div>
+     
     </div>
    
   )
